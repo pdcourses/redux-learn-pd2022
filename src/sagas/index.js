c5c5dc4e60490a06869aa01af2106176edf45936
pdcourses <freshitpt@gmail.com>
2022-08-07 10:46:34 +0300

    user data saga was added

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,3 +1,7 @@
 import {userDataSaga} from './userDataSaga';
-function *rootSaga(){}
+import ACTION_TYPES from '../actions/actionTypes';
+import {takeLatest} from 'redux-saga/effects'
+function *rootSaga(){
+    yield takeLatest(ACTION_TYPES.GET_USER_ACTION,userDataSaga);
+}
 export default rootSaga;
--- src/sagas/userDataSaga.js
+++ src/sagas/userDataSaga.js
@@ -1,2 +1,13 @@
-function *userDataSaga(){}
+import {getUserRequest, getUserError, getUserSuccess} from '../actions/';
+import {put} from 'redux-saga/effects'
+
+function *userDataSaga(action){
+    yield put(getUserRequest())
+    try{
+        const data = yield fetch('https://randomuser.me/api').then(data => data.json())
+        yield put(getUserSuccess(data.results))
+    } catch(error){
+        yield put(getUserError(error))
+    }
+}
 export default userDataSaga;
